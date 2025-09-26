54242cf112e5897bb75788b34c82baa435af1cce
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2025-09-26 11:10:16 -0300

    feat: add biblioteca OOP

---
+++ aula26_09/Biblioteca.java
@@ -0,0 +1,11 @@
+package aula26_09;
+
+public class Biblioteca {
+	public void adicionarLivro(Livro l) {
+		adicionarLivro(l);
+		System.out.println("Livro adicionado com sucesso " + l.getTitulo());
+	}
+	public void emprestarLivro() {
+		
+	}
+}
