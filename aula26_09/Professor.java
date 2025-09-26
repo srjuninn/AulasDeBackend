c47e2458e17228377c27f122a6c8ce03dd3f1cca
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2025-09-26 11:11:57 -0300

    feat: add professor logic OOP

---
+++ aula26_09/Professor.java
@@ -0,0 +1,17 @@
+package aula26_09;
+
+public class Professor extends Usuario {
+	private int limiteEmprestimos = 5;
+
+//	construtor do usuário
+	public Professor(int id, String nome, String email) {
+		super(id, nome, email);
+		// TODO Auto-generated constructor stub
+	}
+
+// 	ultilizando o método abstrato
+	@Override
+	public void getLimiteEmprestimos() {
+		System.out.println("Você tem um limite de " + this.limiteEmprestimos + " emprestimos");
+	}
+}
